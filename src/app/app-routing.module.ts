869efd3ff2613e25d49271e4ce461ec7020bceb6
mauhieu12 <mauhieu12@gmail.com>
2022-09-25 13:08:55 +0700

    add Material library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
+import {IndexComponent} from "./features/index/index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/user/user.module').then(m => m.UserModule)
+  }
+  // {
+  //   path: '',
+  //   component: IndexComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
