f7702993fc68e5a6ebf77f56a89802e1a669016e
Hieu Mau <mauhieu12@gmail.com>
2022-11-07 19:02:07 +0700

    add new collapse new sidenav

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {MediaMatcher} from "@angular/cdk/layout";
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {NavItem} from "../../shared/model/nav-item";
 
 @Component({
   selector: 'app-user',
@@ -6,11 +8,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  showFiller: boolean = false;
 
-  constructor() { }
+
+  menu: NavItem [] = [
+    {
+      displayName: 'UserName',
+      iconName: 'account_circle',
+      children: [
+        {
+          displayName: 'Profile',
+          iconName: 'info',
+          route: 'user-profile'
+        },
+        {
+          displayName: 'Logout',
+          iconName: 'exit_to_app',
+          route: 'logout'
+        }
+      ]
+    },
+    {
+      displayName: 'Dashboard',
+      iconName: 'dashboard',
+      route: 'dashboard'
+    },
+    {
+      displayName: 'Fund Raising Activity',
+      iconName: 'local_activity',
+      children: [
+        {
+          displayName: 'Public Activity',
+          iconName: 'public',
+          route: '/fund-activity/public'
+        },
+        {
+          displayName: 'Private Activity',
+          iconName: 'lock',
+          route: '/fund-activity/private'
+        },
+        {
+          displayName: 'Create Activity',
+          iconName: 'create',
+          route: '/fund-activity/create'
+        },
+      ]
+    },
+    {
+      displayName: 'Group',
+      iconName: 'group',
+      children: [
+        {
+          displayName: 'Owned Group',
+          iconName: 'gavel',
+          route: '/group/owned'
+        },
+        {
+          displayName: 'Joined Group',
+          iconName: 'group_work',
+          route: '/group/joined'
+        },
+        {
+          displayName: 'Create Group',
+          iconName: 'create',
+          route: '/group/create'
+        }
+      ]
+    }
+  ];
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -8,14 +8,18 @@ import {MatCardModule} from "@angular/material/card";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatDialogModule} from "@angular/material/dialog";
+import {MatExpansionModule} from "@angular/material/expansion";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import {MatListModule} from "@angular/material/list";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatTableModule} from "@angular/material/table";
 import {MatTabsModule} from "@angular/material/tabs";
+import {MatToolbarModule} from "@angular/material/toolbar";
 import {BrowserModule} from "@angular/platform-browser";
 import {SharedModule} from "../../shared/shared.module";
 
@@ -79,7 +83,11 @@ import { DonorItemComponent } from '../../shared/component/donor-item/donor-item
     MatCardModule,
     MatGridListModule,
     MatProgressBarModule,
-    SharedModule
+    SharedModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatExpansionModule
   ],
   providers: []
 })
