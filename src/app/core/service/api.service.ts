d7378d38231e26b7aface90717f8a4eac6bb7c34
Hieu Mau <mauhieu12@gmail.com>
2022-11-22 15:01:53 +0700

    Add api service

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -11,27 +11,27 @@ export class ApiService {
   }
 
 
-  // public get<T>(url: string, item?: string, params?: HttpParams): Observable<ApiResponse<T>> {
-  //   if (item) {
-  //     url += `/${item}`;
-  //   }
-  //   return this.httpClient.get<ApiResponse<T>>(url, {params: params});
-  // }
-  //
-  // public post<T>(url: string, item: any): Observable<ApiResponse<T>> {
-  //   return this.httpClient.post<ApiResponse<T>>(url, item);
-  // }
-  //
-  // public put<T>(url: string, item: T): Observable<ApiResponse<T>> {
-  //   return this.httpClient.put<ApiResponse<T>>(url, item);
-  // }
-  //
-  // public delete<T>(url: string, itemId: string): Observable<ApiResponse<T>> {
-  //   url += `/${itemId}`;
-  //   return this.httpClient.delete<ApiResponse<T>>(url);
-  // }
-  //
-  // public getHttpClient(): HttpClient {
-  //   return this.httpClient;
-  // }
+  public get<T>(url: string, item?: string, params?: HttpParams): Observable<T> {
+    if (item) {
+      url += `/${item}`;
+    }
+    return this.httpClient.get<T>(url, {params: params});
+  }
+
+  public post<T>(url: string, item: any): Observable<T> {
+    return this.httpClient.post<T>(url, item);
+  }
+
+  public put<T>(url: string, item: T): Observable<T> {
+    return this.httpClient.put<T>(url, item);
+  }
+
+  public delete<T>(url: string, itemId: string): Observable<T> {
+    url += `/${itemId}`;
+    return this.httpClient.delete<T>(url);
+  }
+
+  public getHttpClient(): HttpClient {
+    return this.httpClient;
+  }
 }
